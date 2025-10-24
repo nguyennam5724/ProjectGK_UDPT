96eeaee388498e5129687f6463145c949515c013
DuyLong <22010378@st.phenikaa-uni.edu.vn>
2025-10-24 07:41:40 +0700

    chore: initialize base project structure

---
+++ src/lib/typesense.ts
@@ -0,0 +1,16 @@
+import Typesense from 'typesense';
+
+export const typesense = new Typesense.Client({
+  apiKey: process.env.NEXT_PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY || 'xyz',
+  nodes: [
+    {
+      host: process.env.NEXT_PUBLIC_TYPESENSE_HOST || 'localhost',
+      port: parseInt(process.env.NEXT_PUBLIC_TYPESENSE_PORT || '8108'),
+      protocol: process.env.NEXT_PUBLIC_TYPESENSE_PROTOCOL || 'http',
+    },
+  ],
+  connectionTimeoutSeconds: 60 * 60,
+});
+
+
+// Commit 2: integrate Typesense for text queries (by Long)
